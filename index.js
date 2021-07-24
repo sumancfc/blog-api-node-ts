94b35d387b917dacad02b66adc1327e65745b16a
Suman Shrestha <sumanstha999@gmail.com>
2021-07-24 22:15:04 +0545

    database connected

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 require("dotenv").config();
 
+//app
 const app = express();
+
+//databse connect
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database Connected!!!"))
+  .catch((err) => console.log(err));
 
 //middlewares
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
+//cors
+if (process.env.NODE_ENV === "development")
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
 
 //routes
 app.get("/", (req, res) => {
