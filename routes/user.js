f061de8bb5d417e19a11de8676a3d3fbf520eeb8
Suman Shrestha <sumanstha999@gmail.com>
2021-08-09 17:15:38 +0545

    user profile api added

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
+const { requireSignin, authMiddleware } = require("../controllers/auth");
 const {
-  requireSignin,
-  authMiddleware,
-  adminMiddleware,
-} = require("../controllers/auth");
-const { read } = require("../controllers/user");
+  read,
+  userProfile,
+  updateUserProfile,
+  getUserPhoto,
+} = require("../controllers/user");
 
 router.get("/profile", requireSignin, authMiddleware, read);
+router.get("/user/:username", userProfile);
+router.put("/profile", requireSignin, authMiddleware, updateUserProfile);
+router.get("/user/photo/:username", getUserPhoto);
 
 module.exports = router;
