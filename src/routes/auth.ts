bd76e5fcfcbaedbc9f014064795dfdbc8093bb8e
sumancfc <sumanstha999@gmail.com>
2024-12-25 14:54:28 +0100

    js files renamed to ts

---
+++ src/routes/auth.ts
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+const { signup, signin, signout } = require("../controllers/auth");
+
+const { runValidation } = require("../../validators");
+const {
+  userSignupValidation,
+  userSigninValidation,
+} = require("../validators/auth");
+
+router.post("/signup", userSignupValidation, runValidation, signup);
+router.post("/signin", userSigninValidation, runValidation, signin);
+router.get("/signout", signout);
+
+module.exports = router;
